08d17a53ab0c36ad383b186cfc3e0b9b587fd3e1
Rob Cameron <rob.cameron@fastmail.com>
2019-09-09 10:22:32 -0700

    Adds postcss-nesting plugin

--- templates/postcss.config.js
+++ templates/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
+    require("postcss-nesting"),
     require("autoprefixer"),
     process.env.NODE_ENV === "production" &&
       require("@fullhuman/postcss-purgecss")({
